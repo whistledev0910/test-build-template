3f424d2abca2721862fdcd7cc9ce4c4022be6cf9
Hai Nguyen <nguyen.nh@hubcom.tech>
2023-12-29 15:35:39 +0700

    init

---
+++ src/templates/temp-1/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+import * as React from "react";
+
+interface ITemplate1Props {}
+
+const Template1: React.FunctionComponent<ITemplate1Props> = (props) => {
+  return <div>Template - 1</div>;
+};
+
+export default Template1;
